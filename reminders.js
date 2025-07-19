5e2ff91b6d1caf49480e0a01e9b21bf28def22f9
azofix <147785092+azofix@users.noreply.github.com>
2025-07-19 16:57:02 +0200

    gitignore added and config.js

---
+++ reminders.js
@@ -0,0 +1,17 @@
+const { exec } = require ("child_process");
+const { REMINDERS_LIST } =require("./config.js");
+
+function addReminder(text) { 
+
+    const script =
+      tell application "Reminders"
+        make new reminder in list "${REMINDERS_LIST}" with properties {name:"${text}"}
+      end tell 
+
+return new Promise((resolve, reject) => {
+    exec(`osascript -e '${script}'`, (err) => {
+        if (err) reject(err);
+        else resolve(`Àdded "${text}" to ${REMINDERS_LIST}`);
+        });
+    });
+}
